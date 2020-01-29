095ab80135f1fb921d726aa8f184e89c74d1b39b
Tim Brady <Tim_brady1@homedepot.com>
2020-01-28 19:39:03 -0500

    feat: initial setup of api service

--- src/app/services/jump-cloud-api.service.ts
+++ src/app/services/jump-cloud-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,31 @@ export class JumpCloudApiService {
 
   getUser(id: string) {
     return this.http.get(`api/systemusers/${id}`);
+  }
+
+  addUser(user: Omit<User, 'id'>) {
+    const data = {
+      op: 'add',
+      type: 'system_group',
+      username: user.userName,
+      email: user.email
+    };
+
+    return this.http.post('api/systemusers', data);
+  }
+
+  deleteUser(id: string) {
+    return this.http.delete(`api/systemuser/${id}`);
+  }
+
+  updateUser(user: User) {
+    const data = {
+      username: user.userName,
+      email: user.email,
+      op: 'update',
+      type: 'system_group'
+    };
+
+    return this.http.put(`api/systemusers/${user.id}`, data);
   }
 }
